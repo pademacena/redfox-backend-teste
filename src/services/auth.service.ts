fe5c4d63ccd6bc2b1c5b80cb9f3e8e5943959524
Paulo Augusto <pademacena@gmail.com>
2023-10-20 14:03:22 -0300

    feat: inicio implementacao authenticate para gerar token de acesso

---
+++ src/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { sign } from 'jsonwebtoken';
+import { BaseService } from './base.service';
+
+export class AuthService extends BaseService {
+  public async authenticate(userName: string) {
+    const token = this.tokenGenerate(userName);
+    return token;
+  }
+
+  private tokenGenerate(user: string) {
+    const token = sign(
+      { user },
+      `${process.env.HASH_TOKEN}`,
+      {
+        expiresIn: '1d'
+      }
+    );
+    return token;
+  }
+}
